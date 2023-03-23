defe89aa810d6fbcbf1135a3aba06a09e45e3c1b
Igor <igara8880@gmail.com>
2023-03-23 18:16:47 +0300

    project45

--- Project45/Source.cpp
+++ Project45/Source.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<ctime>
 #include<string>
+#define DEFAULT 27
 
 using namespace std;
 void init(int a) {
@@ -14,9 +15,11 @@ void init(int a) {
 
 int main() {
 	srand(time(NULL));
-	int num1, num2, num3;
-	int sum;
 	do {
+		int* num1 = new int[DEFAULT];
+		int* num2 = new int[DEFAULT];
+		int* num3 = new int[DEFAULT];
+		int* sum = new int[DEFAULT];
 		init(num1);
 		init(num2);
 		init(num3);
@@ -25,6 +28,7 @@ int main() {
 		cout << "It is correct" << endl;
 		system("cls");
 	} while (num1 + num2 + num3 == sum);
+	srand(time(NULL));
 
 
 
